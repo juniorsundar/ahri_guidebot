45ba23fe459ea549b871628144514c5ac53c2ac2
Junior Sundar <juniorsundar@gmail.com>
2020-04-03 16:14:36 -0600

    added findClosest in graph function and updated AStar with human arm cost function

--- src/MotionPlanner.py
+++ src/MotionPlanner.py
@@ -5,9 +5,9 @@ import copy
 import random
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from shapely.geometry import LineString
-from shapely.geometry.polygon import Polygon
+from shapely.geometry import Polygon
+from shapely.geometry import Point as Points
 from shapely import affinity
 import os
 
@@ -19,6 +19,8 @@ g_limb.set_joint_position_speed(.1)
 workspace = np.array([[0,0.7],[-0.7,0.7],[0,0.2]])
 graph = []
 quat = Quaternion(0.704238785359,0.709956638597,-0.00229009932359,0.00201493272073)
+start_point_robot = Point(0.0,0.0,0.1)
+start_point_human = Point(0.7,0,0.1)
 
 obs_c = np.array([[-0.5,0.25455],[-0.3,0.54091],[0,0.38182],[0.35,0.54091],[0.4,0.25455]])
 obs_w = 0.05
@@ -148,37 +150,42 @@ def isInObstacle(newNode):
         if x > c[0] and x < c[2] and y > c[1] and y < c[3]:
             isIn = True
     return isIn
-
-def intersectsObstacle(line):
-    intersects = False
-    lx,ly = line.coords
-    scaledObs = []
-    scaledLine = LineString([(10*lx[0],100*ly[0]),(10*lx[1],100*ly[1])])
-    for c in obstacles:
-        scaledObs.append(affinity.scale(c,xfact=10,yfact=100))
-    for c in scaledObs:
-        y,x = c.exterior.xy
-        ly,lx = scaledLine.coords
-        if scaledLine.intersects(c):
-            plt.plot(x,y)
-            plt.plot(lx,ly)
-            plt.show()
-            intersects = True
-    return intersects
     
+def safeForHuman(index):
+    global graph, start_point_human, obstacles
+    p3 = Points(start_point_human.y,start_point_human.x)
+    for i in reversed(range(len(graph[index].neighbours))):
+        neighbour = graph[graph[index].neighbours[i]]
+        p1 = Points(graph[index].pose.position.y,graph[index].pose.position.x)
+        p2 = Points(neighbour.pose.position.y,neighbour.pose.position.x)
+        poly = Polygon([p1,p2,p3])
+        for k in obstacles:
+            if k.intersects(poly):
+                graph[index].neighbours.pop(i)
+
+def humanCost(current, neighbour):
+    global graph, start_point_human, obstacles
+    cost = 0
+    p3 = Points(start_point_human.y,start_point_human.x)
+    p1 = Points(current.Node.pose.position.y,current.Node.pose.position.x)
+    p2 = Points(neighbour.Node.pose.position.y,neighbour.Node.pose.position.x)
+    poly = Polygon([p1,p2,p3])
+    for k in obstacles:
+        if k.intersects(poly):
+            cost = 100
+    return cost
 
 def RRT():
-    global graph, quat
-    start_point = Point(0.7,0.0,0.1)
+    global graph, quat, start_point_robot
 
     start = Pose()
-    start.position = copy.deepcopy(start_point)
+    start.position = copy.deepcopy(start_point_robot)
     start.orientation = copy.deepcopy(quat)
 
     target_joint_angles = g_limb.ik_request(start, "right_hand")
     start = Node(start,target_joint_angles)
     graph.append(start)
-    for i in range(0,600):
+    for i in range(0,700):
         XNew = GenerateRandom()
         while isInObstacle(XNew):
             XNew = GenerateRandom()
@@ -187,20 +194,26 @@ def RRT():
         graph[i+1].Link(XNearestIdx)
         graph[XNearestIdx].Link(i+1)
     os.system('clear')
+
+    #clear starting point neighbours to remove huge jumps
     for i in range(len(graph[0].neighbours)):
         graph[graph[0].neighbours[i]].neighbours.pop(0)
     graph[0].neighbours = []
-    for i in range(0,len(graph)):
+
+
+    for i in range(len(graph)):
         if graph[0].eDist(graph[i]) < 0.1:
             graph[0].Link(i)
             graph[i].Link(0)
+
     fig = plt.figure()
-    for i in range(0,len(graph)):
+    for i in range(len(graph)):
         if i == 0:
             plt.scatter(graph[i].pose.position.x,graph[i].pose.position.y,c='g',marker='o')
         else:
             plt.scatter(graph[i].pose.position.x,graph[i].pose.position.y,c='r',marker='x')
-        for j in range(0,len(graph[i].neighbours)):
+        for j in range(len(graph[i].neighbours)):
+            # print j
             Lx = np.array([graph[i].pose.position.x,graph[graph[i].neighbours[j]].pose.position.x])
             Ly = np.array([graph[i].pose.position.y,graph[graph[i].neighbours[j]].pose.position.y])
             Lz = np.array([graph[i].pose.position.z,graph[graph[i].neighbours[j]].pose.position.z])
@@ -208,7 +221,7 @@ def RRT():
         for c in obstacles:
             y,x = c.exterior.xy
             plt.plot(x,y)
-
+    fig.savefig('plot.png')
     plt.show()
     
 def reconstruct_path(current):
@@ -220,15 +233,20 @@ def reconstruct_path(current):
         total_path[i] = total_path[i].Node
     return total_path
     
-def A_Star(start,goal,Graph):
+def A_Star(start,goal):
+    global graph
     graph2 = []
-    for node in Graph:
+    for node in graph:
         graph2.append(Vertex(node))
     
+    for i in range(len(graph2)):
+        graph2[i].g = 100
+
     openSet = [graph2[start]]
     closedSet = []
-    
-    openSet[0].f = openSet[0].Node.distanceTo(graph2[goal].Node)
+
+    openSet[0].f = openSet[0].Node.eDist(graph2[goal].Node)
+    openSet[0].g = 0
     openSet[0].previous = openSet[0]
     
     while len(openSet)>0:
@@ -239,7 +257,7 @@ def A_Star(start,goal,Graph):
                 winner = i
         current = openSet[winner]
 
-        if current == graph2[goal]:
+        if current.Node.eDist(graph2[goal].Node) < 0.01:
             return reconstruct_path(current)
         
         openSet.pop(winner)
@@ -247,40 +265,64 @@ def A_Star(start,goal,Graph):
         
         for ni in current.Node.neighbours:
             neighbour = graph2[ni]
-            
-            if neighbour in closedSet:
-                continue
-                
-            tentG = current.g + current.Node.distanceTo(neighbour.Node)
-            
-            if neighbour not in openSet:
-                openSet.append(neighbour)
-            elif tentG >= neighbour.g:
-                continue
-            neighbour.previous = current
-            neighbour.g = tentG
-            neighbour.f = neighbour.g + neighbour.Node.distanceTo(Vertex(graph[goal]).Node)
-                        
-    return reconstruct_path(current)     
 
+            tentG = current.g + current.Node.eDist(neighbour.Node) + humanCost(current,neighbour)
+            
+            if tentG < neighbour.g:
+                neighbour.previous = current
+                neighbour.g = tentG
+                neighbour.f = neighbour.g +  + current.Node.eDist(graph2[goal].Node)
+                if neighbour not in closedSet:
+                    openSet.append(neighbour)
+    return "No Path"                  
+    # return reconstruct_path(current)     
+
+def findClosest(check):
+    global graph
+    closestIndex = 0
+    dist = 0.1
+    for i in range(len(graph)):
+        ax=check.x
+        ay=check.y
+        az=check.z
+        bx=graph[i].pose.position.x
+        by=graph[i].pose.position.y
+        bz=graph[i].pose.position.z
+        nuDist = np.linalg.norm(np.array([ax,ay,az])-np.array([bx,by,bz]))
+        if nuDist < dist:
+            dist = nuDist
+            closestIndex = i
+    return closestIndex
+    
 
 if __name__ == "__main__":
     RRT()
-    # A = A_Star(0,600,graph)
-    print(graph[600].pose.position)
-    # for i in range(0,len(graph)):
-    #     if i == 0:
-    #         plt.scatter(graph[i].pose.position.x,graph[i].pose.position.y,c='g',marker='o')
-    #     else:
-    #         plt.scatter(graph[i].pose.position.x,graph[i].pose.position.y,c='r',marker='x')
-    #     for j in range(0,len(graph[i].neighbours)):
-    #         Lx = np.array([graph[i].pose.position.x,graph[graph[i].neighbours[j]].pose.position.x])
-    #         Ly = np.array([graph[i].pose.position.y,graph[graph[i].neighbours[j]].pose.position.y])
-    #         plt.plot(Lx,Ly,c='k')
-    # for i in range(1,len(A)):
-    #     Lx = np.array([A[i-1].pose.position.x,A[i].pose.position.x])
-    #     Ly = np.array([A[i-1].pose.position.y,A[i].pose.position.y])
-    #     Lz = np.array([A[i-1].pose.position.z,A[i].pose.position.z])
-    #     plt.plot(Lx,Ly,c='r')
+    while True:
+        print 'Start'
+        starting = Point(float(raw_input('x: ')),float(raw_input('y: ')),float(raw_input('z: ')))
+        print 'Ending'
+        ending = Point(float(raw_input('x: ')),float(raw_input('y: ')),float(raw_input('z: ')))
+        si = findClosest(starting)
+        ei = findClosest(ending)
+        print(si, ei)
+        A = A_Star(si,ei)
+        if (type(A) is not str):
+            for i in range(0,len(graph)):
+                if i == 0:
+                    plt.scatter(graph[i].pose.position.x,graph[i].pose.position.y,c='g',marker='o')
+                else:
+                    plt.scatter(graph[i].pose.position.x,graph[i].pose.position.y,c='r',marker='x')
+                for j in range(0,len(graph[i].neighbours)):
+                    Lx = np.array([graph[i].pose.position.x,graph[graph[i].neighbours[j]].pose.position.x])
+                    Ly = np.array([graph[i].pose.position.y,graph[graph[i].neighbours[j]].pose.position.y])
+                    plt.plot(Lx,Ly,c='k')
+            for i in range(1,len(A)):
+                Lx = np.array([A[i-1].pose.position.x,A[i].pose.position.x])
+                Ly = np.array([A[i-1].pose.position.y,A[i].pose.position.y])
+                Lz = np.array([A[i-1].pose.position.z,A[i].pose.position.z])
+                plt.plot(Lx,Ly,c='r')
+            for c in obstacles:
+                y,x = c.exterior.xy
+                plt.plot(x,y)
 
-    # plt.show()
+            plt.show()
